25361404bd1a5c96a58f93ca5f9559d04efa0fdc
donaldnkala <donaldnkala7@gmail.com>
2026-07-03 05:36:29 -0700

    Add files via upload

---
+++ grade checker.py
@@ -0,0 +1,14 @@
+marks = input("enter your marks:")
+marks = int(marks)
+
+if marks >= 80:
+    print("grade A")
+
+elif marks >= 60:
+    print("grade B")
+
+elif marks >= 50:
+    print("grade C")
+
+else:
+    print("fail")
